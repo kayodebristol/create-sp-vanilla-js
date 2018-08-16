289ffb6f2b424000a32b473455ead9ea3f5d11da
kayodebristol <kayode.bristol@gmail.com>
2018-08-16 09:06:06 +0430

    Style Changes

--- index.js
+++ index.js
@@ -3,27 +3,23 @@
 let shell = require("shelljs");
 let colors = require("colors");
 let fs = require("fs"); //fs already comes included with node.
-
+let proc = require("child_process"); 
 let appName = process.argv[2];
 let appDirectory = `${process.cwd()}/${appName}`;
 
 let rootTemplates = require("./templates/rootTemplates.js");
 
 
-const CreateParcelApp = () => {
+const installParcel = () => {
   return new Promise(resolve => {
     if (appName) {
-      shell.exec("npm i parcel -g", () => {
-        shell.exec(`mkdir ${appName}`, () => {
-          console.log("Created app directory");
-          shell.cd(`${appName}`), () =>{
-            shell.exec(`git init`), ()=>{
-              shell.exec(`npm init`)
-            }
-          }
+      if(!shell.which("parcel")){
+        shell.exec("npm i parcel-bundler -g", () => {
           resolve(true);
         });
-      });
+      } else{
+        resolve(true);
+      }
     } else {
       console.log("\nNo app name was provided.".red);
       console.log("\nProvide an app name in the following format: ");
@@ -32,6 +28,22 @@ const CreateParcelApp = () => {
     }
   });
 };
+
+const gitInit = ()=>{
+  return new Promise((resolve, reject)=>{
+    if(shell.which("git")){
+      proc.execFileSync("git", ["init"], {stdio: "inherit", shell: true}); 
+    }
+    resolve(true); 
+  })
+}
+
+const npmInit = ()=>{
+  return new Promise((resolve, reject)=>{
+    proc.execFileSync("npm", ["init"], {stdio: "inherit", shell: true}); 
+    resolve(true); 
+  })
+}
 
 const cdIntoNewApp = () => {
   return new Promise(resolve => {
@@ -45,7 +57,11 @@ const cdIntoNewApp = () => {
 const makeDirectories = () => {
   return new Promise(resolve => {
     shell.exec(
-      `mkdir config`,
+      `mkdir ${appName}`,
+      console.log("\nCreated application folder\n".green)
+    );
+    shell.exec(
+      `cd ${appName} && mkdir config`,
       console.log("\nCreated sp-rest-proxy config folder\n".green)
     );
     resolve(true);
@@ -78,16 +94,16 @@ const updateTemplates = (template, path) => {
 const installPackages = () => {
   return new Promise(resolve => {
     console.log(
-      "\nInstalling @pnp/common @pnp/graph, @pnp/logging, @pnp/odata, @pnp/sp, datejs, js-logger babel-ployfill babel-runtime es6-promise isomorphic-fetch \n"
+      "\nInstalling @pnp/common @pnp/graph @pnp/logging @pnp/odata @pnp/sp datejs js-logger babel-polyfill babel-runtime es6-promise isomorphic-fetch"
         .cyan
     );
     shell.exec(
-      'npm i @pnp/common @pnp/graph @pnp/logging @pnp/odata @pnp/sp datejs js-logger babel-ployfill babel-runtime es6-promise isomorphic-fetch',
+      "\nnpm i @pnp/common @pnp/graph @pnp/logging @pnp/odata @pnp/sp datejs js-logger babel-polyfill babel-runtime es6-promise isomorphic-fetch",
       () => {
         console.log("\nFinished installing default packages\n".green);
         console.log("\nInstalling dev dependencies \n".cyan);
         shell.exec(
-          "npm i --save-dev babel-preset-env sp-rest-proxy concurrently babel-plugin-js-logger babel-ployfill babel-runtime es6-promise isomorphic-fetch",
+          "npm i --save-dev  sp-rest-proxy concurrently babel-preset-env babel-plugin-js-logger babel-core babel-plugin-transform-runtime",
           () => {
             console.log("\nFinished installing dev dependencies\n".green);
             resolve();
@@ -127,15 +143,27 @@ const updatePackageJSON = (filename, updateCb, cb) => {
 };
 
 const run = async () => {
-  let success = await CreateParcelApp();
+
+  let success = await installParcel();
   if (!success) {
     console.log(
-      "Something went wrong while trying to create a new React app using create-react-app"
+      "\nSomething went wrong while trying to install parcel-bundler"
         .red
     );
     return false;
   }
-
+  
+  await makeDirectories();
+  await cdIntoNewApp();
+  await gitInit(); 
+  success = await npmInit(); //proc.execFileSync('npm', ['init'], {stdio: 'inherit'}); 
+  if(!success){
+    console.log(
+      "\nSomething went wrong runing npm init!"
+        .red
+    );
+    return false;
+  }
   await updatePackageJSON(
     `${appDirectory}/package.json`,
     data => {
@@ -143,10 +171,10 @@ const run = async () => {
       data = Object.assign({ proxy: "http://localhost:8080" }, data);
       data.scripts = Object.assign(
         {
-          start: "parcel index.html --no-cache",
+          start: "parcel index.html --no-cache --open",
           proxy: "node ./api-server.js",
-          startServers: "concurrently --kill-others \"npm run proxy\" \"npm run start\"",
-          build: "parcel build index.js --out-dir {TargetDir} --out-file {FileName}" 
+          dev: "concurrently --kill-others \"npm run proxy\" \"npm run start\"",
+          build: "parcel build index.html " 
         },
         data.scripts
       );
@@ -154,13 +182,29 @@ const run = async () => {
       return data;
     },
     () => {
-      console.log("Error updating package.json");
+      console.log("\nError updating package.json");
     }
   );
-  await cdIntoNewApp();
-  await makeDirectories();
+
   await updateTemplates(rootTemplates, "");
   await installPackages();
-  console.log("All done!");
+
+  console.log("\nAll done!");
+  
+  console.log("\nThis starter kit uses SharePoint Patterns and Practices (pnp) to retrieve SharePoint Data."
+  .blue); 
+  console.log("\nsp-rest-proxy allows communication with SharePoint while developing in nodejs."
+  .blue); 
+  console.log("\nGetting Started: "
+  .green); 
+  console.log("\n1. CD, into your project directory. Update your JavaScript in index.js. Html in index.html, the entry point for your solution, can also be used to set any global variables (handly when targeting CEWPs). index."
+  .bold); 
+  console.log("\n2. Execute, npm run proxy, then answer the interactive questions to configure the proxy connection to your SharePoint site. Ctrl-c to end task."
+  .bold); 
+  console.log("\n3. Execute, npm run dev (uses concurrently), to start the proxy and dev server simultaneously."
+  .bold); 
+  console.log("\n4. Develop interactively, with real SharePoint data. Enjoy!"
+  .bold);
+
 };
 run();
--- templates/index.js
+++ templates/index.js
@@ -1,9 +1,11 @@
 module.exports = `
 import 'babel-polyfill'; 
 import 'isomorphic-fetch';
+import Logger from 'js-logger';
 import { sp } from "@pnp/sp";
 
 (async function(){
+    Logger.useDefaults(); 
     require('es6-promise').polyfill();
     sp.setup({
         sp: {
@@ -13,14 +15,19 @@ import { sp } from "@pnp/sp";
             headers: {
                 Accept: "application/json;odata=verbose",
             },
+            baseUrl: "Url of SP Web or SP Site"
         },
     });
- 
+    Logger.debug(sp); 
+    
     const today = new Date(); 
+    Logger.debug(today); 
 
+
+    /* Set baseUrl, above, before uncommenting this section or using pnp
     let siteInfo = await sp.web.get()
-    Logger.useDefaults(); 
-    Logger.debug("Host: " + siteInfo.Url + " Site Name: " + siteInfo.Title + " Description: " + siteInfo.Description) ;
+    Logger.info(today + " :: Host: " + siteInfo.Url + " Site Name: " + siteInfo.Title + " Description: " + siteInfo.Description) ;
+    */
   
 })();
 `
